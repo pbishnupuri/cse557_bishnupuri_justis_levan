3d376ddba5fa81e0ca052255e92306c1ec5c814e
Kynan Justis <kfjustis@gmail.com>
2022-02-09 18:03:01 -0600

    add data files and map setup

--- project.js
+++ project.js
@@ -1,14 +1,26 @@
-var data = [80, 120, 60, 150, 200];
+//var data = [80, 120, 60, 150, 200];
+//
+//var barHeight = 20;
+//
+//var bar = d3.select('svg')
+//          .selectAll('rect')
+//          .data(data)
+//          .enter()
+//          .append('rect')
+//          .attr('width', function(d) {  return d; })
+//          .attr('height', barHeight - 1)
+//          .attr('transform', function(d, i) {
+//            return "translate(0," + i * barHeight + ")";
+//          });
 
-var barHeight = 20;
+const mapContainer = d3.select("#map-container");
+const mapPath = "./data/MC2-tourist.jpg";
+mapContainer.append("img")
+  .attr("id", "map-img")
+  .attr("src", mapPath)
+  .attr("width", 800)
+  .attr("height", 600);
 
-var bar = d3.select('svg')
-          .selectAll('rect')
-          .data(data)
-          .enter()
-          .append('rect')
-          .attr('width', function(d) {  return d; })
-          .attr('height', barHeight - 1)
-          .attr('transform', function(d, i) {
-            return "translate(0," + i * barHeight + ")";
-          });
+//d3.csv("./data/gps.csv")
+//  .row(function(d) { return {key: d.key, value: d.value}; })
+//  .get(function(error, rows) { console.log(rows); });
